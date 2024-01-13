9b5f29922fd4b0231c02e3e5c46f6d722ac6a1b7
t1k2a <maxtj1122@gmail.com>
2024-01-13 11:40:05 +0900

    API経由でDB保存する

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import express from 'express'
-import morgan from 'morgan'
 import 'express-async-errors'
+import morgan from 'morgan'
+import mysql from 'mysql2/promise'
+
+const EMPTY = 0
+const DARK = 1
+const LIGHT = 2
+
+const INITIAL_BOARD = [
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
+  ]
 
 const PORT = 3000
 
@@ -8,14 +24,72 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('static', { extensions: ['html'] }))
+
 app.get('/api/hello', async (req, res) => {
     res.json({
         message: 'hello express!!!'
     })
 })
 
-app.get('/api/error', async(req, res) => {
+app.get('/api/error', async (req, res) => {
     throw new Error('Error endpoint')
+})
+
+app.post('/api/games', async (req, res) => {
+    const now = new Date()
+
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        database: 'reversi',
+        user: 'reversi',
+        password: 'password'
+    })
+    try {
+        await conn.beginTransaction()
+
+        const gameInsertResult = await conn.execute<mysql.ResultSetHeader>(
+            'insert into games (started_at) values (?)',
+             [now]
+             )
+        const gameId = gameInsertResult[0].insertId
+
+        const turnInsertResult = await conn.execute<mysql.ResultSetHeader>(
+            'insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)',
+            [gameId, 0, DARK, now]
+        )
+        const turnId = turnInsertResult[0].insertId
+
+        // 行ごとの要素数を足し算している
+        const squareCount = INITIAL_BOARD.map((line) => line.length).reduce(
+            (v1, v2) => v1 + v2,
+            0
+        )
+
+        const squaresInsertSql =
+         'insert into squares (turn_id, x, y, disc) values ' +
+         Array.from(Array(squareCount))
+         .map(() => '(?, ?, ?, ?)')
+         .join(', ')
+
+        const squaresInsertValues: any[] = []
+        // 2重ループによってX軸とY軸を取得できる
+        INITIAL_BOARD.forEach((line, y) => {
+            line.forEach((disc, x) => {
+                squaresInsertValues.push(turnId)
+                squaresInsertValues.push(x)
+                squaresInsertValues.push(y)
+                squaresInsertValues.push(disc)
+            })
+        })
+
+        await conn.execute(squaresInsertSql, squaresInsertValues)
+
+        await conn.commit()
+    } finally {
+        await conn.end()
+    }
+
+    res.status(201).end()
 })
 
 app.use(errorHandler)
@@ -27,6 +101,6 @@ app.listen(PORT, () => {
 function errorHandler(err: any, _req: express.Request, res: express.Response,  _next: express.NextFunction) {
     console.log('Unexpected error occurred', err)
     res.status(500).send({
-        messagve: 'Unexpexted error occured'
+        message: 'Unexpected error occurred'
     })
 }
