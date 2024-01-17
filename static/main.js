70fdb89f8c8244ad2b6909eb62ef14a6274c3bad
t1k2a <maxtj1122@gmail.com>
2024-01-17 09:24:46 +0900

    盤面取得のフロントエンド実装

--- static/main.js
+++ static/main.js
@@ -1,53 +1,48 @@
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
 
-const INITIAL_BOARD = [
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,DARK,LIGHT,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,LIGHT,DARK,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY]
-]
-
-const boardElement = document.getElementById('board')
+const boardElement = document.getElementById("board");
 
 async function showBoard() {
-    while (boardElement.firstChild) {
-        boardElement.removeChild(boardElement.firstChild)
-    }
+  const turnCount = 0;
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
 
-    INITIAL_BOARD.forEach((line) => {
-        line.forEach((square) => {
-            // <div class="square"></div>
-            const squareElement = document.createElement('div')
-            squareElement.className = 'square'
+  while (boardElement.firstChild) {
+    boardElement.removeChild(boardElement.firstChild);
+  }
 
-            if (square !== EMPTY) {
-                // div class="stone dark"
-                const stoneElement = document.createElement('div')
-                const color = square === DARK ? 'dark' : 'light'
-                stoneElement.className = `stone ${color}`
+  board.forEach((line) => {
+    line.forEach((square) => {
+      // <div class="square">
+      const squareElement = document.createElement("div");
+      squareElement.className = "square";
 
-                squareElement.appendChild(stoneElement)
-            }
+      if (square !== EMPTY) {
+        // <div class="stone dark">
+        const stoneElement = document.createElement("div");
+        const color = square === DARK ? "dark" : "light";
+        stoneElement.className = `stone ${color}`;
 
-            boardElement.appendChild(squareElement)
-        })
-    })
+        squareElement.appendChild(stoneElement);
+      }
+
+      boardElement.appendChild(squareElement);
+    });
+  });
 }
 
 async function registerGame() {
-    await fetch('api/games', {
-        method: 'POST'
-    })
-}
-async function main() {
-    await registerGame()
-    await showBoard()
+  await fetch("/api/games", {
+    method: "POST",
+  });
 }
 
-main()
+async function main() {
+  await registerGame();
+  await showBoard();
+}
+
+main();
