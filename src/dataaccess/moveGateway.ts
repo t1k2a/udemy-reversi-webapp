c868b6c390f41517d6ba76932dcc03d87291029e
t1k2a <maxtj1122@gmail.com>
2024-02-07 00:03:47 +0900

    リポジトリパターン導入

--- src/dataaccess/moveGateway.ts
+++ src/dataaccess/moveGateway.ts
@@ -1,6 +1,30 @@
-import mysql from 'mysql2/promise'
+import mysql from "mysql2/promise";
+import { MoveRecord } from "./moveRecord";
 
 export class MoveGateway {
+  async findForTurnId(
+    conn: mysql.Connection,
+    turnId: number
+  ): Promise<MoveRecord | undefined> {
+    const moveSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      "select id, turn_id, disc, x, y from moves where turn_id = ?",
+      [turnId]
+    );
+
+    const record = moveSelectResult[0][0];
+
+    if (!record) {
+      return undefined;
+    }
+
+    return new MoveRecord(
+      record["id"],
+      record["turn_id"],
+      record["disc"],
+      record["x"],
+      record["y"]
+    );
+  }
   async insert(
     conn: mysql.Connection,
     turnId: number,
@@ -9,8 +33,8 @@ export class MoveGateway {
     y: number
   ) {
     await conn.execute(
-      'insert into moves (turn_id, disc, x, y) values (?, ?, ?, ?)',
+      "insert into moves (turn_id, disc, x, y) values (?, ?, ?, ?)",
       [turnId, disc, x, y]
-    )
+    );
   }
 }
--- src/domain/turn.ts
+++ src/domain/turn.ts
@@ -1,7 +1,7 @@
-import { Board } from './board'
-import { Disc } from './disc'
-import { Move } from './move'
-import { Point } from './point'
+import { Board } from "./board";
+import { Disc } from "./disc";
+import { Move } from "./move";
+import { Point } from "./point";
 
 export class Turn {
   constructor(
@@ -16,15 +16,15 @@ export class Turn {
   placeNext(disc: Disc, point: Point): Turn {
     // 打とうとした石が、次の石ではない場合、置くことはできない
     if (disc !== this._nextDisc) {
-      throw new Error('Invalid disc')
+      throw new Error("Invalid disc");
     }
 
-    const move = new Move(disc, point)
+    const move = new Move(disc, point);
 
-    const nextBoard = this._board.place(move)
+    const nextBoard = this._board.place(move);
 
     // TODO 次の石が置けない場合はスキップする処理
-    const nextDisc = disc === Disc.Dark ? Disc.Light : Disc.Dark
+    const nextDisc = disc === Disc.Dark ? Disc.Light : Disc.Dark;
 
     return new Turn(
       this._gameId,
@@ -33,26 +33,30 @@ export class Turn {
       move,
       nextBoard,
       new Date()
-    )
+    );
   }
 
   get gameId() {
-    return this._gameId
+    return this._gameId;
   }
 
   get turnCount() {
-    return this._turnCount
+    return this._turnCount;
   }
 
   get nextDisc() {
-    return this._nextDisc
+    return this._nextDisc;
+  }
+
+  get move() {
+    return this._move;
   }
 
   get board() {
-    return this._board
+    return this._board;
   }
 
   get endAt() {
-    return this._endAt
+    return this._endAt;
   }
 }
