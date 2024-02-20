7b3ba2dc653504fe934529e537343ea18cd76451
t1k2a <maxtj1122@gmail.com>
2024-02-20 22:40:32 +0900

    ひっくり返す処理

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,20 +1,26 @@
-import { Disc } from "./disc";
+import { Disc, isOppositeDisc } from "./disc";
 import { Move } from "./move";
 import { Point } from "./point";
 
 export class Board {
-  constructor(private _discs: Disc[][]) {}
+  private _walledDiscs: Disc[][];
+
+  constructor(private _discs: Disc[][]) {
+    this._walledDiscs = this.wallDiscs();
+  }
 
   place(move: Move): Board {
-    // TODO 盤面に置けるかチェック
+    // 空のマス目ではない場合、置くことはできない
     if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
-      throw new Error("selected point is not empty");
+      throw new Error("Selected point is not empty");
     }
 
-    const flipPoints = this.listFlipPoints();
+    // ひっくり返せる点をリストアップ
+    const flipPoints = this.listFlipPoints(move);
 
+    // ひっくり返せる点がない場合、置くことはできない
     if (flipPoints.length === 0) {
-      throw new Error("flip points is empty");
+      throw new Error("Flip points is empty");
     }
 
     // 盤面をコピー
@@ -28,12 +34,74 @@ export class Board {
     newDiscs[move.point.y][move.point.x] = move.disc;
 
     // TODO ひっくり返す
-
+    flipPoints.forEach((p) => {
+      newDiscs[p.y][p.x] = move.disc;
+    });
     return new Board(newDiscs);
   }
 
-  private listFlipPoints(): Point[] {
-    return [new Point(0, 0)];
+  private listFlipPoints(move: Move): Point[] {
+    const flipPoints: Point[] = [];
+
+    const walledX = move.point.x + 1;
+    const walledY = move.point.y + 1;
+
+    const checkFlipPoints = (xMove: number, yMove: number) => {
+      const flipCandidate: Point[] = [];
+
+      // 1つ動いた位置から開始
+      let cursorX = walledX + xMove;
+      let cursorY = walledY + yMove;
+
+      // 手と逆の色の石がある間、1つずつを見ていく
+      while (isOppositeDisc(move.disc, this._walledDiscs[cursorY][cursorX])) {
+        // 番兵を考慮して-1する
+        flipCandidate.push(new Point(cursorX - 1, cursorY - 1));
+        cursorX += xMove;
+        cursorY += yMove;
+        // 次が手と同じ色の石なら、ひっくり返す石が確定
+        if (move.disc === this._walledDiscs[cursorY][cursorX]) {
+          flipPoints.push(...flipCandidate);
+          break;
+        }
+      }
+    };
+
+    // 上
+    checkFlipPoints(0, -1);
+    // 左上
+    checkFlipPoints(-1, -1);
+    // 左
+    checkFlipPoints(-1, 0);
+    // 左下
+    checkFlipPoints(-1, 1);
+    // 下
+    checkFlipPoints(0, 1);
+    // 右下
+    checkFlipPoints(1, 1);
+    // 右
+    checkFlipPoints(1, 0);
+    // 右上
+    checkFlipPoints(1, -1);
+
+    return flipPoints;
+  }
+
+  private wallDiscs(): Disc[][] {
+    const walled: Disc[][] = [];
+
+    const topAndBottomWall = Array(this._discs[0].length + 2).fill(Disc.Wall);
+
+    walled.push(topAndBottomWall);
+
+    this._discs.forEach((line) => {
+      const walledLine = [Disc.Wall, ...line, Disc.Wall];
+      walled.push(walledLine);
+    });
+
+    walled.push(topAndBottomWall);
+
+    return walled;
   }
 
   get discs() {
@@ -56,4 +124,4 @@ const INITIAL_DISCS = [
   [E, E, E, E, E, E, E, E],
 ];
 
-export const initial_board = new Board(INITIAL_DISCS);
+export const initialBoard = new Board(INITIAL_DISCS);
--- src/domain/turn/disc.ts
+++ src/domain/turn/disc.ts
@@ -1,11 +1,19 @@
 export const Disc = {
   Empty: 0,
   Dark: 1,
-  Light: 2
-} as const
+  Light: 2,
+  wall: 3,
+} as const;
 
-export type Disc = typeof Disc[keyof typeof Disc]
+export type Disc = (typeof Disc)[keyof typeof Disc];
 
 export function toDisc(value: number): Disc {
-  return value as Disc
+  return value as Disc;
+}
+
+export function isOppositeDisc(disc1: Disc, disc2: Disc): boolean {
+  return (
+    (disc1 === Disc.Dark && disc2 === Disc.Light) ||
+    (disc1 === Disc.Light && disc2 === Disc.Dark)
+  );
 }
