f3ffd0ddf37587c5851e85412b3f0d4b6e973721
t1k2a <maxtj1122@gmail.com>
2024-03-07 10:07:19 +0900

    QueryServiceを使ったAPIの実装

--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { StartNewGameUseCase } from "../application/useCase/startNewGameUseCase";
 import { GameMySQLRepository } from "../infrastructure/repository/game/gameMySQLRepository";
 import { TurnMySQLRepository } from "../infrastructure/repository/turn/turnMySQLRepository";
+import { FindLastGameUseCase } from "../application/useCase/findLastGameUseCase";
+import { FindLastGameMySQLQueryService } from "../infrastructure/repository/query/findlLastGamesMySQLQueryService";
 
 export const gameRouter = express.Router();
 
@@ -10,6 +12,42 @@ const startNewGameUseCase = new StartNewGameUseCase(
   new TurnMySQLRepository()
 );
 
+const findLastGameUseCase = new FindLastGameUseCase(
+  new FindLastGameMySQLQueryService()
+);
+
+interface GetGamesResponseBody {
+  games: {
+    id: number;
+    darkMoveCount: number;
+    lightMoveCount: number;
+    winnerDisc: number;
+    startedAt: Date;
+    endAt: Date;
+  }[];
+}
+
+gameRouter.get(
+  "/api/games",
+  async (req, res: express.Response<GetGamesResponseBody>) => {
+    const output = await findLastGameUseCase.run();
+    const responseBodyGames = output.map((g) => {
+      return {
+        id: g.gameId,
+        darkMoveCount: g.darkMoveCount,
+        lightMoveCount: g.lightMoveCount,
+        winnerDisc: g.winnerDisc,
+        startedAt: g.startedAt,
+        endAt: g.endAt,
+      };
+    });
+    const responseBody = {
+      games: responseBodyGames,
+    };
+
+    res.json(responseBody);
+  }
+);
 gameRouter.post("/api/games", async (req, res) => {
   await startNewGameUseCase.run();
 
